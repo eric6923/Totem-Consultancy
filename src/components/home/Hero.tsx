790621f3df89ab1bb2ffdd89d37050a2b48d31f7
eric6923 <eric.2002@srit.org>
2025-01-08 23:17:19 +0530

    Form Sending Mail Change

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -14,7 +14,7 @@ export default function Hero() {
     }
   }, []);
 
-  const handleSubmit = (e:any) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
 
     const form = e.target;
@@ -31,12 +31,12 @@ export default function Hero() {
     };
 
     emailjs
-    .send(
-      "service_52wf8so", // Replace with your EmailJS service ID
-      "template_l889yux", // Replace with your EmailJS template ID
-      templateParams,
-      "4uNPbbh4mrwY7kIYa" // Replace with your EmailJS public key
-    )
+      .send(
+        "service_52wf8so", // Replace with your EmailJS service ID
+        "template_l889yux", // Replace with your EmailJS template ID
+        templateParams,
+        "4uNPbbh4mrwY7kIYa" // Replace with your EmailJS public key
+      )
       .then(
         (result) => {
           console.log("Email successfully sent!", result.text);
@@ -61,7 +61,10 @@ export default function Hero() {
         loop
         playsInline
       >
-        <source src="https://res.cloudinary.com/dgagkq1cs/video/upload/v1736333395/loading_hfjoq1.mp4" type="video/mp4" />
+        <source
+          src="https://res.cloudinary.com/dgagkq1cs/video/upload/v1736333395/loading_hfjoq1.mp4"
+          type="video/mp4"
+        />
         Your browser does not support the video tag.
       </video>
 
@@ -71,8 +74,10 @@ export default function Hero() {
           <div className="max-w-3xl mt-0 lg:mt-16 text-center lg:text-left">
             <h1 className="text-4xl sm:text-5xl lg:text-7xl font-bold mb-6 leading-tight">
               <span className="block lg:hidden">
-                We Empower<br />
-                Brands and<br />
+                We Empower
+                <br />
+                Brands and
+                <br />
                 Individuals
               </span>
               <span className="hidden lg:block">
@@ -81,14 +86,18 @@ export default function Hero() {
             </h1>
             <div className="text-base sm:text-lg text-gray-300 mb-8 max-w-2xl mx-auto lg:mx-0">
               <p className="lg:hidden">
-                A one-stop shop for the solution of<br />
-                Digital Marketing, Content Creation,<br />
-                Graphics, Animation, and Customized<br />
+                A one-stop shop for the solution of
+                <br />
+                Digital Marketing, Content Creation,
+                <br />
+                Graphics, Animation, and Customized
+                <br />
                 Courses that demonstrate individuals and brands.
               </p>
               <p className="hidden lg:block">
-                A one-stop shop for the solution of Digital Marketing, Content Creation, 
-                Graphics, Animation, and Customized Courses that demonstrate individuals and brands.
+                A one-stop shop for the solution of Digital Marketing, Content
+                Creation, Graphics, Animation, and Customized Courses that
+                demonstrate individuals and brands.
               </p>
             </div>
             <div className="flex justify-center lg:justify-start">
@@ -108,71 +117,83 @@ export default function Hero() {
       {showForm && (
         <div className="">
           <div className=" top-1/2 m-auto right-[20px] -translate-y-1/2 h-[550px] w-[90%] lg:w-[400px] bg-white z-[999] overflow-y-auto transition-transform duration-300 transform rounded-l-lg absolute ">
-          <button 
-            onClick={() => setShowForm(false)}
-            className="absolute top-4 right-4 text-black hover:text-gray-600"
-          >
-            ✕
-          </button>
-          <form className="p-4 mt-10" onSubmit={handleSubmit}>
-            <div className="space-y-4">
-              <div>
-                <label htmlFor="name" className="block text-sm text-black mb-1">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  className="w-full px-3 py-2 bg-gray-100 border border-gray-200 rounded-lg focus:ring-2 focus:ring-gray-200 focus:border-transparent outline-none transition-all text-black"
-                  required
-                />
-              </div>
+            <button
+              onClick={() => setShowForm(false)}
+              className="absolute top-4 right-4 text-black hover:text-gray-600"
+            >
+              ✕
+            </button>
+            <form className="p-4 mt-10" onSubmit={handleSubmit}>
+              <div className="space-y-4">
+                <div>
+                  <label
+                    htmlFor="name"
+                    className="block text-sm text-black mb-1"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    className="w-full px-3 py-2 bg-gray-100 border border-gray-200 rounded-lg focus:ring-2 focus:ring-gray-200 focus:border-transparent outline-none transition-all text-black"
+                    required
+                  />
+                </div>
 
-              <div>
-                <label htmlFor="email" className="block text-sm text-black mb-1">
-                  Email Address
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  className="w-full px-3 py-2 bg-gray-100 border border-gray-200 rounded-lg focus:ring-2 focus:ring-gray-200 focus:border-transparent outline-none transition-all text-black"
-                  required
-                />
-              </div>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-sm text-black mb-1"
+                  >
+                    Email Address
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="w-full px-3 py-2 bg-gray-100 border border-gray-200 rounded-lg focus:ring-2 focus:ring-gray-200 focus:border-transparent outline-none transition-all text-black"
+                    required
+                  />
+                </div>
 
-              <div>
-                <label htmlFor="service" className="block text-sm text-black mb-1">
-                  Service you are Interested in
-                </label>
-                <input
-                  type="text"
-                  id="service"
-                  className="w-full px-3 py-2 bg-gray-100 border border-gray-200 rounded-lg focus:ring-2 focus:ring-gray-200 focus:border-transparent outline-none transition-all text-black"
-                  required
-                />
-              </div>
+                <div>
+                  <label
+                    htmlFor="service"
+                    className="block text-sm text-black mb-1"
+                  >
+                    Service you are Interested in
+                  </label>
+                  <input
+                    type="text"
+                    id="service"
+                    className="w-full px-3 py-2 bg-gray-100 border border-gray-200 rounded-lg focus:ring-2 focus:ring-gray-200 focus:border-transparent outline-none transition-all text-black"
+                    required
+                  />
+                </div>
 
-              <div>
-                <label htmlFor="message" className="block text-sm text-black mb-1">
-                  Message
-                </label>
-                <textarea
-                  id="message"
-                  rows={3}
-                  className="w-full px-3 py-2 bg-gray-100 border border-gray-200 rounded-lg focus:ring-2 focus:ring-gray-200 focus:border-transparent outline-none transition-all text-black resize-none"
-                  required
-                ></textarea>
-              </div>
+                <div>
+                  <label
+                    htmlFor="message"
+                    className="block text-sm text-black mb-1"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    rows={3}
+                    className="w-full px-3 py-2 bg-gray-100 border border-gray-200 rounded-lg focus:ring-2 focus:ring-gray-200 focus:border-transparent outline-none transition-all text-black resize-none"
+                    required
+                  ></textarea>
+                </div>
 
-              <button
-                type="submit"
-                className="w-full bg-black text-white px-6 py-2 text-lg font-medium rounded-lg hover:bg-gray-900 transition-colors"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
+                <button
+                  type="submit"
+                  className="w-full bg-black text-white px-6 py-2 text-lg font-medium rounded-lg hover:bg-gray-900 transition-colors"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       )}
     </div>
