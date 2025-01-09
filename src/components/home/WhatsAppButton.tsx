38ba286c829380b47312fdd47ba3e682768b1dea
eric6923 <eric.2002@srit.org>
2025-01-09 14:40:00 +0530

    Close in WhatsApp button Fixed

--- src/components/home/WhatsAppButton.tsx
+++ src/components/home/WhatsAppButton.tsx
@@ -14,22 +14,22 @@ const WhatsAppButton = () => {
   return (
     <div className="fixed bottom-3 left-0 z-50">
       {showChat && (
-        <div className="relative inline-flex items-center ml-4">
-          <a 
-            className="bg-white md:px-4 md:py-2 px-3 py-1 rounded-2xl border-black border-1 shadow-[3px_3px_0_#22c55e] mr-4 "
-            href="https://wa.me/918278416000?text=Hey,%20is%20anyone%20available%20for%20a%20chat?%20I%20need%20to%20know%20about%20your%20services."
-          >
-            Chat with us
-          </a>
-          <button
-            onClick={() => setShowChat(false)}
-            className="bg-black rounded-full p-1 text-white hover:bg-gray-200 transition-colors"
-            aria-label="Close chat prompt"
-          >
-            <X size={16} />
-          </button>
-        </div>
-      )}
+  <div className="relative inline-flex items-center ml-4">
+    <button
+      onClick={() => setShowChat(false)}
+      className="bg-black rounded-full p-1 text-white hover:bg-gray-200 transition-colors mr-4"
+      aria-label="Close chat prompt"
+    >
+      <X size={16} />
+    </button>
+    <a 
+      className="bg-white md:px-4 md:py-2 px-3 py-1 rounded-2xl border-black border-1 shadow-[3px_3px_0_#22c55e]"
+      href="https://wa.me/918278416000?text=Hey,%20is%20anyone%20available%20for%20a%20chat?%20I%20need%20to%20know%20about%20your%20services."
+    >
+      Chat with us
+    </a>
+  </div>
+)}
       <div className='text-left ml-4'>
         <button
           onClick={handleWhatsAppClick}
