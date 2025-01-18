8a84cb23012ca637400176dfcf8756669b87fe85
eric6923 <eric.2002@srit.org>
2025-01-18 15:37:33 +0530

    Setting changes Api

--- frontend/src/components/Crm/Admin/Settings.tsx
+++ frontend/src/components/Crm/Admin/Settings.tsx
@@ -71,7 +71,7 @@ const SettingsPage = () => {
           {showDesignation && (
             <div>
               <label className="block text-sm font-medium text-gray-700 dark:text-gray-200 mb-1">
-                Designation
+                Designatio
               </label>
               <input
                 type="text"
